d35f5e9fb792b1abb7e8fd7b9b6b132087bcac0a
zamk1d <minecrafttvem@gmail.com>
2026-08-31 19:22:54 +0200

    imports fix

--- alembic/env.py
+++ alembic/env.py
@@ -6,6 +6,7 @@ from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 from src.core.config import settings
 from src.database.db import Base
+from src.models.models import Booking
 from alembic import context
 
 # this is the Alembic Config object, which provides
