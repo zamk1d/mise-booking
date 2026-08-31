ca4a31db44bef54d1616d7c508b1e45340e4fa64
zamk1d <minecrafttvem@gmail.com>
2026-08-31 15:40:20 +0200

    configure async database connection

---
+++ src/repository/database/db.py
@@ -0,0 +1,19 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.orm import DeclarativeBase
+from src.core.config import settings
+from collections.abc import AsyncGenerator
+
+class Base(DeclarativeBase):
+    pass
+
+
+engine = create_async_engine(settings.database_url)
+AsyncSessionLocal = async_sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False
+)
+
+async def get_db() -> AsyncGenerator[AsyncSession, None]:
+    async with AsyncSessionLocal() as session:
+        yield session
