6a3f66e73cd13ef81e571861384a2ebcaa4ab79c
zamk1d <minecrafttvem@gmail.com>
2026-08-31 18:31:04 +0200

    import paths fix

--- alembic/env.py
+++ alembic/env.py
@@ -5,7 +5,7 @@ from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 from src.core.config import settings
-from database.db import Base
+from src.database.db import Base
 from alembic import context
 
 # this is the Alembic Config object, which provides
