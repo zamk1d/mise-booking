25a233de8c150affaef43e547019497a7e33f011
zamk1d <minecrafttvem@gmail.com>
2026-08-31 17:56:49 +0200

    added 'change status' & 'get by datetime' methods and added date parameter for 'list' method

--- src/repository/booking_repository.py
+++ src/repository/booking_repository.py
@@ -1,3 +1,6 @@
+from datetime import date, time
+from enum import Enum
+
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -14,10 +17,25 @@ class BookingRepository:
         )
         return result.scalar_one_or_none()
 
-    async def list(self, limit: int = 50, offset: int = 0) -> list[Booking]:
+    async def get_by_datetime(self, booking_date: date, booking_time: time):
         result = await self.session.execute(
-            select(Booking).limit(limit).offset(offset)
+            select(Booking).where(
+                Booking.booking_date == booking_date,
+                Booking.booking_time == booking_time,
+                Booking.status == Enum("active")
+            )
         )
+        return result.scalar_one_or_none()
+
+    async def list(self, booking_date: date | None = None, limit: int = 50, offset: int = 0) -> list[Booking]:
+        if booking_date is None:
+            result = await self.session.execute(
+                select(Booking).limit(limit).offset(offset)
+            )
+        else:
+            result = await self.session.execute(
+                select(Booking).where(Booking.booking_date == booking_date).limit(limit).offset(offset)
+            )
         return list(result.scalars().all())
 
     async def create(self, **kwargs) -> Booking:
@@ -26,5 +44,7 @@ class BookingRepository:
         await self.session.commit()
         return booking
 
-    async def delete(self, booking: Booking) -> None:
-        await self.session.delete(booking)
+    async def change_status(self, booking: Booking, status) -> Booking:
+        booking.status = status
+        await self.session.commit()
+        return booking
